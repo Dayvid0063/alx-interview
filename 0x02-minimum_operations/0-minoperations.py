b901c74ca24c3316e579044532efd6a1607727c8
Dayvid0063 <orjidavid63@gmail.com>
2024-06-12 12:51:26 +0100

    ops update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,21 +5,11 @@
 def minOperations(n):
     """Method that calculates the fewest number of operations
     needed to result in exactly n H characters"""
-    if not isinstance(n, int) or n <= 1:
-        return 0
-    
     ops = 0
-    cur = 1
-    c_len = 0
-
-    while cur < n:
-        if c_len == 0:
-            c_len = cur
-            ops += 1
-        if (n - cur) % cur == 0:
-            c_len = cur
-            ops += 1
-        cur += c_len
-        ops += 1
-
+    cur = 2
+    while n > 1:
+        while n % cur == 0:
+            ops += cur
+            n /= cur
+        cur += 1
     return ops
